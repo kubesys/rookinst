8ee19e852f27e58eef7358f858dc790d6f3e3c4f
syswu <wuheng@iscas.ac.cn>
2025-03-04 15:43:18 +0800

    Revert "Update rook_cleanup.sh"
    This reverts commit 1fed8c295b5329dea30e5340f885378e7d022246.

--- rook_cleanup.sh
+++ rook_cleanup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # delete top of the Rook cluster
-# delete the ceph object store
-kubectl -n rook-ceph delete cephobjectstore my-store
 
-for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
-kubectl get -n rook-ceph "$CRD" -o name | 
-xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
-done
+
 # delete the CephCluster CRD
-# kubectl -n rook-ceph patch cephcluster rook-ceph --type merge -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
-# kubectl -n rook-ceph delete cephcluster rook-ceph
-kubectl delete -f cluster/cluster.yaml
+kubectl -n rook-ceph patch cephcluster rook-ceph --type merge -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
+kubectl -n rook-ceph delete cephcluster rook-ceph
 rm -rf /var/lib/rook
 
 # Wait for CephCluster to be deleted
@@ -23,8 +17,6 @@ done
 # delete the Operator and related Resources
 kubectl delete -f cluster/operator.yaml
 kubectl delete -f cluster/common.yaml
-kubectl patch -n rook-ceph cm/rook-ceph-mon-endpoints --type merge -p '{"metadata":{"finalizers": []}}'
-kubectl patch -n rook-ceph secret/rook-ceph-mon --type merge -p '{"metadata":{"finalizers": []}}'
 kubectl delete -f cluster/crds.yaml
 # kubectl delete -f cluster/dashboard-exporter.yaml
 
